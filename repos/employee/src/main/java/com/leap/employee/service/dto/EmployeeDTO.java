22abbf9e81427b37a964e6bb928e20db22a18aa1
Nam <NamTran2010>
2024-10-29 14:13:59 +0700

    Create Table Jobhistory of 1 employee

--- repos/employee/src/main/java/com/leap/employee/service/dto/EmployeeDTO.java
+++ repos/employee/src/main/java/com/leap/employee/service/dto/EmployeeDTO.java
@@ -5,6 +5,9 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Objects;
 import javax.validation.constraints.*;
+import java.util.Set;
+import java.util.HashSet;
+import com.leap.employee.domain.JobHistory;
 
 /**
  * A DTO for the {@link com.leap.employee.domain.Employee} entity.
@@ -28,6 +31,8 @@ public class EmployeeDTO implements Serializable {
     private JobDTO job;
 
     private DepartmentDTO department;
+
+    private Set<JobHistory> jobHistories = new HashSet<>();
 
     public Long getId() {
         return id;
@@ -86,37 +91,86 @@ public class EmployeeDTO implements Serializable {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
+    public boolean equals(Object obj) {
+        if (this == obj)
             return true;
-        }
-        if (!(o instanceof EmployeeDTO)) {
+        if (obj == null)
             return false;
-        }
+        if (getClass() != obj.getClass())
+            return false;
+        EmployeeDTO other = (EmployeeDTO) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (hireDate == null) {
+            if (other.hireDate != null)
+                return false;
+        } else if (!hireDate.equals(other.hireDate))
+            return false;
 
-        EmployeeDTO employeeDTO = (EmployeeDTO) o;
-        if (this.id == null) {
+        if (endDate == null) {
+            if (other.endDate != null)
+                return false;
+        } else if (!endDate.equals(other.endDate))
             return false;
-        }
-        return Objects.equals(this.id, employeeDTO.id);
+
+        if (salary == null) {
+            if (other.salary != null)
+                return false;
+        } else if (!salary.equals(other.salary))
+            return false;
+        if (job == null) {
+            if (other.job != null)
+                return false;
+        } else if (!job.equals(other.job))
+            return false;
+        if (department == null) {
+            if (other.department != null)
+                return false;
+        } else if (!department.equals(other.department))
+            return false;
+        if (jobHistories == null) {
+            if (other.jobHistories != null)
+                return false;
+        } else if (!jobHistories.equals(other.jobHistories))
+            return false;
+        return true;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.id);
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((hireDate == null) ? 0 : hireDate.hashCode());
+        result = prime * result + ((endDate == null) ? 0 : endDate.hashCode());
+        result = prime * result + ((salary == null) ? 0 : salary.hashCode());
+        result = prime * result + ((job == null) ? 0 : job.hashCode());
+        result = prime * result + ((department == null) ? 0 : department.hashCode());
+        result = prime * result + ((jobHistories == null) ? 0 : jobHistories.hashCode());
+        return result;
     }
 
-    // prettier-ignore
     @Override
     public String toString() {
-        return "EmployeeDTO{" +
-            "id=" + getId() +
-            ", name='" + getName() + "'" +
-            ", hireDate='" + getHireDate() + "'" +
-            ", endDate='" + getEndDate() + "'" +
-            ", salary=" + getSalary() +
-            ", job=" + getJob() +
-            ", department=" + getDepartment() +
-            "}";
+        return "EmployeeDTO [id=" + id + ", name=" + name + ", hireDate=" + hireDate + ", endDate=" + endDate
+                + ", salary=" + salary + ", job="
+                + job + ", department=" + department + ", jobHistories=" + jobHistories + "]";
+    }
+
+    public Set<JobHistory> getJobHistories() {
+        return jobHistories;
+    }
+
+    public void setJobHistories(Set<JobHistory> jobHistories) {
+        this.jobHistories = jobHistories;
     }
 }
--- repos/gateway/src/main/webapp/app/entities/employee/employee/detail/employee-detail.component.ts
+++ repos/gateway/src/main/webapp/app/entities/employee/employee/detail/employee-detail.component.ts
@@ -2,15 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { IEmployee } from '../employee.model';
+import { EmployeeService } from '../service/employee.service';
 
 @Component({
   selector: 'jhi-employee-detail',
   templateUrl: './employee-detail.component.html',
+  styleUrls: ['./employee-detail.component.scss'],
 })
 export class EmployeeDetailComponent implements OnInit {
   employee: IEmployee | null = null;
+  jobHistories: any[] = [];
+  showJobHistory = false;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute, private employeeService: EmployeeService) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ employee }) => {
@@ -20,5 +24,19 @@ export class EmployeeDetailComponent implements OnInit {
 
   previousState(): void {
     window.history.back();
+  }
+
+  handleClick(): void {
+    this.showJobHistory = !this.showJobHistory;
+    if (!this.showJobHistory) {
+      return;
+    }
+
+    const employeeId = this.employee?.id;
+    if (employeeId) {
+      this.employeeService.getJobHistoriesForEmployee(employeeId).subscribe(jobHistory => {
+        this.jobHistories = jobHistory;
+      });
+    }
   }
 }
