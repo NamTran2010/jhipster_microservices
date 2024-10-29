3fdd31654f5be21c3f849b9fd8b0078207980c65
Nam <NamTran2010>
2024-10-29 17:17:07 +0700

    Add Delete button into detail document

--- repos/gateway/src/main/webapp/app/entities/employee/document/detail/document-detail.component.ts
+++ repos/gateway/src/main/webapp/app/entities/employee/document/detail/document-detail.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { combineLatest } from 'rxjs';
 
 import { IDocument } from '../document.model';
+import { DocumentDeleteDialogComponent } from '../delete/document-delete-dialog.component';
+import { ITEMS_PER_PAGE, DESC, ASC, SORT } from 'app/config/pagination.constants';
+import { DocumentService } from '../service/document.service';
+import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'jhi-document-detail',
@@ -9,16 +16,102 @@ import { IDocument } from '../document.model';
 })
 export class DocumentDetailComponent implements OnInit {
   document: IDocument | null = null;
+  documents?: IDocument[];
+  isLoading = false;
+  totalItems = 0;
+  itemsPerPage = ITEMS_PER_PAGE;
+  page?: number;
+  predicate!: string;
+  ascending!: boolean;
+  ngbPaginationPage = 1;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(
+    protected activatedRoute: ActivatedRoute,
+    protected documentService: DocumentService,
+    protected router: Router,
+    protected modalService: NgbModal,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ document }) => {
       this.document = document;
     });
   }
+  loadPage(page?: number, dontNavigate?: boolean): void {
+    this.isLoading = true;
+    const pageToLoad: number = page ?? this.page ?? 1;
 
+    this.documentService
+      .query({
+        page: pageToLoad - 1,
+        size: this.itemsPerPage,
+        sort: this.sort(),
+      })
+      .subscribe(
+        (res: HttpResponse<IDocument[]>) => {
+          this.isLoading = false;
+          this.onSuccess(res.body, res.headers, pageToLoad, !dontNavigate);
+        },
+        () => {
+          this.isLoading = false;
+          this.onError();
+        }
+      );
+  }
   previousState(): void {
     window.history.back();
+  }
+
+  delete(document: IDocument): void {
+    const modalRef = this.modalService.open(DocumentDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.document = document;
+    // Unsubscribe is not needed as closed completes on modal close
+    modalRef.closed.subscribe(reason => {
+      if (reason === 'deleted') {
+        this.location.back(); // Go back to the previous page
+      }
+    });
+  }
+
+  protected onSuccess(data: IDocument[] | null, headers: HttpHeaders, page: number, navigate: boolean): void {
+    this.totalItems = Number(headers.get('X-Total-Count'));
+    this.page = page;
+    if (navigate) {
+      this.router.navigate(['/document'], {
+        queryParams: {
+          page: this.page,
+          size: this.itemsPerPage,
+          sort: this.predicate + ',' + (this.ascending ? ASC : DESC),
+        },
+      });
+    }
+    this.documents = data ?? [];
+    this.ngbPaginationPage = this.page;
+  }
+
+  protected handleNavigation(): void {
+    combineLatest([this.activatedRoute.data, this.activatedRoute.queryParamMap]).subscribe(([data, params]) => {
+      const page = params.get('page');
+      const pageNumber = +(page ?? 1);
+      const sort = (params.get(SORT) ?? data['defaultSort']).split(',');
+      const predicate = sort[0];
+      const ascending = sort[1] === ASC;
+      if (pageNumber !== this.page || predicate !== this.predicate || ascending !== this.ascending) {
+        this.predicate = predicate;
+        this.ascending = ascending;
+        this.loadPage(pageNumber, true);
+      }
+    });
+  }
+  protected sort(): string[] {
+    const result = [this.predicate + ',' + (this.ascending ? ASC : DESC)];
+    if (this.predicate !== 'id') {
+      result.push('id');
+    }
+    return result;
+  }
+  protected onError(): void {
+    this.ngbPaginationPage = this.page ?? 1;
   }
 }
